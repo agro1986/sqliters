e787601dfb927a99012539a344340319d2ceecef
Agro Rachmatullah <agro1986@gmail.com>
2019-04-07 03:21:32 +0900

    basic repl skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::process;
+use std::io;
+use std::io::Write;
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_prompt();
+        let mut input_buffer = String::new();
+        io::stdin().read_line(&mut input_buffer).unwrap();
+        let input_buffer = input_buffer.trim();
+        if input_buffer == ".exit" {
+            process::exit(libc::EXIT_SUCCESS);
+        } else {
+            println!("Unrecognized command '{}'.", input_buffer);
+
+
+        }
+
+    }
+
 }
